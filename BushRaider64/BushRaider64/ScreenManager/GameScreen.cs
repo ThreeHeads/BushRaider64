ea6cd197c2585d3dbcb4e391ec23f1836943b67c
CodingDolphin <codingdolphin@gmail.com>
2014-07-14 20:28:53 +0200

    Ground Project

---
+++ BushRaider64/BushRaider64/ScreenManager/GameScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BushRaider64
+{
+    abstract class GameScreen
+    {
+        public event ScreenHandler screenChangeHandler;
+
+        abstract public void LoadContent();
+
+        abstract public void Update();
+
+        abstract public void Draw();
+    }
+}
---
+++ BushRaider64/BushRaider64/ScreenManager/ScreenManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BushRaider64
+{
+    public delegate void ScreenHandler(string input);
+
+    class ScreenManager
+    {
+    }
+}
