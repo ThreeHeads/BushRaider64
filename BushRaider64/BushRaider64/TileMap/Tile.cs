0fbb4a7b409a5f096b0ca9a1e943d0dfdec6eedf
Charlie <falke_88@hotmail.com>
2014-07-14 22:49:13 +0200

    Tile.cs Update

--- BushRaider64/BushRaider64/TileMap/Tile.cs
+++ BushRaider64/BushRaider64/TileMap/Tile.cs
@@ -5,11 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using SFML.Graphics;
 using SFML.Window;
+using SFML.Audio;
 
-namespace BushRaider64.TileMap
+namespace BushRaider64
 {
     class Tile
     {
+        RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Tile_Test");
+
         
+
     }
 }
