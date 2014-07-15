15543810acfae1e3c8708d9bb661e5d1a1b36450
Charlie <falke_88@hotmail.com>
2014-07-15 21:29:50 +0200

    TileMap.cs Update #2

---
+++ BushRaider64/BushRaider64/TileMap/TileMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.Audio;
+
+namespace BushRaider64
+{
+    class TileMap
+    {
+        private Sprite tile;
+
+        public TileMap(int tile_width, int tile_height)
+        {
+            this.tile = SpriteFactory.createSpriteCentered(new IntRect(0, 0, tile_width, tile_height), "GameAssets/stone_cobble.jpg");
+        }
+
+        public void TileMapDraw(RenderWindow renderWindow)
+        {
+            renderWindow.Draw(tile);
+
+        }
+
+    }
+}
