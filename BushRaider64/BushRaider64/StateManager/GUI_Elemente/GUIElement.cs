31ee0b18b88f2a726e7301942861ffa551e5f371
QuickndDirty <f.fromm88@web.de>
2014-07-15 22:21:34 +0200

    Added new GUIElement

---
+++ BushRaider64/BushRaider64/StateManager/GUI_Elemente/GUIElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BushRaider64.StateManager.GUI_Elemente
+{
+    abstract class GUIElement
+    {
+        public void loadContent();
+        public void draw();
+        public void update();
+    }
+}
